0a0ca45f0bf6f3877f723090b4b5933dfe0315ac
Shuaib <ahamed.shuaib10@gmail.com>
2025-06-07 21:50:03 +0530

    Crop recommendation update

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from sklearn.neighbors import KNeighborsClassifier
 app = Flask(__name__)
 
@@ -29,8 +29,8 @@ def analyse():
         import pandas as pd
         import numpy as np
         #import os
-        #optimum = pd.read_excel(optimum1 file)
-        #price = pd.read_excel(optimum2 file path) 
+        optimum = pd.read_csv("./optimum1.csv")
+        price = pd.read_csv("./optimum2.csv") 
         optimum['N'] = optimum.N.astype(float)
         optimum['P'] = optimum.P.astype(float)
         optimum['K'] = optimum.K.astype(float)
@@ -42,62 +42,20 @@ def analyse():
         clf = KNeighborsClassifier(n_neighbors=3)
         clf.fit(X,y)
 
-        print(request.form.get('Potassium'))
+        data = request.json
+        print(f"Received data: {data}")
+        nitrogen = data.get('nitrogen')
+        phosphorous = data.get('phosphorous')
+        potassium = data.get('potassium')
+        temperature = data.get('temperature')
+        pH = data.get('pH')
 
-        if(request.form.get('Potassium') == None):
+        if not data:
+            return {"error": "All soil parameters required"}
 
-            #pred = pd.read_excel("optimum2path")
-
-            prediction = clf.predict(pred)
-            print(prediction)
-
-            optimum = optimum[optimum['CLASS'] != prediction[0]]
-            X = optimum.drop("CLASS", axis=1)
-            y = optimum.CLASS
-            clf = KNeighborsClassifier(n_neighbors=3)
-            clf.fit(X,y)
-            prediction1 = clf.predict(pred)
-            print(prediction1)
-
-            optimum = optimum[optimum['CLASS'] != prediction1[0]]
-            X = optimum.drop("CLASS", axis=1)
-            y = optimum.CLASS
-            clf = KNeighborsClassifier(n_neighbors=3)
-            clf.fit(X,y)
-            prediction2 = clf.predict(pred)
-            print(prediction2)
-
-            p1 = prediction1[0]
-            p2 = prediction2[0]
-            p1 = p1 -1
-            p2 = p2 -1
-
-            if(prediction == 7):
-                return render_template('crops.html', crop="TOMATO" , crop1=prediction1[0], crop2=prediction2[0])
-            elif(prediction == 1):
-                return render_template('crops.html', crop="GARLIC" , crop1=prediction1[0], crop2=prediction2[0])
-            elif(prediction == 2):
-                return render_template('crops.html', crop="ONION" , crop1=prediction1[0], crop2=prediction2[0])
-            elif(prediction == 3):
-                return render_template('crops.html', crop="ORANGE" , crop1=prediction1[0], crop2=prediction2[0])
-            elif(prediction == 4):
-                return render_template('crops.html', crop="PEAS" , crop1=prediction1[0], crop2=prediction2[0])
-            elif(prediction == 5):
-                return render_template('crops.html', crop="POTATO" , crop1=prediction1[0], crop2=prediction2[0])
-            elif(prediction == 6):
-                return render_template('crops.html', crop="RICE" , crop1=prediction1[0], crop2=prediction2[0])
-            elif(prediction == 8):
-                return render_template('crops.html', crop="SUGARCANE" , crop1=prediction1[0], crop2=prediction2[0])
-            else:
-                return "no"
 
         else:
-            potassium = request.form.get('Potassium')
-            phosphorous = request.form.get('Phosphorous')
-            nitrogen = request.form.get('Nitrogen')
-            pH = request.form.get('pH')
-            temperature = request.form.get('Temperature')
-
+            
             columns = ['N', 'P', 'K', 'pH', 'TEMPERATURE']
             values = np.array([nitrogen, phosphorous, potassium, pH, temperature])
             pred = pd.DataFrame(values.reshape(-1, len(values)), columns=columns)
@@ -105,7 +63,7 @@ def analyse():
             print(pred)
 
             prediction = clf.predict(pred)
-            print(prediction)
+            print(f"prediction : {prediction}")
 
             optimum = optimum[optimum['CLASS'] != prediction[0]]
             X = optimum.drop("CLASS", axis=1)
@@ -113,7 +71,7 @@ def analyse():
             clf = KNeighborsClassifier(n_neighbors=3)
             clf.fit(X,y)
             prediction1 = clf.predict(pred)
-            print(prediction1)
+            print(f"prediction1 : {prediction1}")
 
             optimum = optimum[optimum['CLASS'] != prediction1[0]]
             X = optimum.drop("CLASS", axis=1)
@@ -121,7 +79,7 @@ def analyse():
             clf = KNeighborsClassifier(n_neighbors=3)
             clf.fit(X,y)
             prediction2 = clf.predict(pred)
-            print(prediction2)
+            print(f"prediction2 : {prediction2}")
 
             p1 = prediction1[0]
             p2 = prediction2[0]
@@ -129,23 +87,40 @@ def analyse():
             p2 = p2 -1
 
             if(prediction == 7):
-                return render_template('crops.html', crop="TOMATO" , crop1=prediction1[0], crop2=prediction2[0])
+                return render_template('crops.html', crop="Tomato" , crop1=prediction1[0], crop2=prediction2[0])
+                
             elif(prediction == 1):
-                return render_template('crops.html', crop="GARLIC" , crop1=prediction1[0], crop2=prediction2[0])
+                return render_template('crops.html', crop="Garlic" , crop1=prediction1[0], crop2=prediction2[0])
+                
             elif(prediction == 2):
-                return render_template('crops.html', crop="ONION" , crop1=prediction1[0], crop2=prediction2[0])
+                return render_template('crops.html', crop="Onion" , crop1=prediction1[0], crop2=prediction2[0])
+                
             elif(prediction == 3):
-                return render_template('crops.html', crop="ORANGE" , crop1=prediction1[0], crop2=prediction2[0])
+                return render_template('crops.html', crop="Orange" , crop1=prediction1[0], crop2=prediction2[0])
+                
             elif(prediction == 4):
-                return render_template('crops.html', crop="PEAS" , crop1=prediction1[0], crop2=prediction2[0])
+                return render_template('crops.html', crop="Peas" , crop1=prediction1[0], crop2=prediction2[0])
+                
             elif(prediction == 5):
-                return render_template('crops.html', crop="POTATO" , crop1=prediction1[0], crop2=prediction2[0])
+                return render_template('crops.html', crop="Potato" , crop1=prediction1[0], crop2=prediction2[0])
+                
             elif(prediction == 6):
-                return render_template('crops.html', crop="RICE" , crop1=prediction1[0], crop2=prediction2[0])
+                return render_template('crops.html', crop="Rice" , crop1=prediction1[0], crop2=prediction2[0])
+                
             elif(prediction == 8):
-                return render_template('crops.html', crop="SUGARCANE" , crop1=prediction1[0], crop2=prediction2[0])
-            else:
-                return "no"
+                return render_template('crops.html', crop="Sugarcane" , crop1=prediction1[0], crop2=prediction2[0])
+                
+            
+                
+            
+            return jsonify({
+            "success": True,
+            "redirect": "/crops.html",
+            "message": "Analysis complete"
+        })
+
+        
+    
 
 if (__name__ == "__main__"):
     app.run()
